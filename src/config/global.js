91f60b7bd0604cf5ac3930b9cdd05f0c4e6ce9d8
Selvan S <87435305+Selvan-S@users.noreply.github.com>
2023-12-13 20:36:37 +0530

    added render link via env

--- src/config/global.js
+++ src/config/global.js
@@ -1,4 +1,6 @@
-const API_URL = "https://todolist-be-lrdl.onrender.com";
+import "dotenv/config";
+
+const API_URL = process.env.API_URL;
 // const API_URL = "http://localhost:5000";
 
 export default API_URL;
